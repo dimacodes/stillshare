6d297aa72024dada2358b36f46d0e8f704081ca3
dimitriwashere <dimitriwashere@gmail.com>
2017-04-30 23:25:59 -0400

    Modified event, user serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date, :descritption
+  attributes :id, :title, :date, :description
   belongs_to :user
   has_many :images
 end
