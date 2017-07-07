a0818656cd1ce4ebce98a84f01fc490b625d6daa
dimitriwashere <dimitriwashere@gmail.com>
2017-07-07 13:08:57 -0400

    Modify get, show functions

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,10 +19,10 @@ function getEvents() {
 
       data.user.events.forEach(function(event) {
         // debugger
-        var id = event.id;
+        eventId = event.id;
         var userId = event.user_id;
         var eventLink = $(document.createElement("a"));
-        eventLink.attr('href', "http://localhost:3000/users/" + parseInt(userId) + "/events/" + parseInt(id))
+        eventLink.attr('href', "http://localhost:3000/users/" + parseInt(userId) + "/events/" + parseInt(eventId))
         eventLink.text(event.title)
         $(".all_events").append(eventLink).append("<br>")
       })
@@ -31,36 +31,37 @@ function getEvents() {
 }
 
 // fix below to dyna route, display images
-// function showEvent(e) {
-//   $("a.all_events").on("click", function(e) {
-//     e.preventDefault();
-//     $.ajax({
-//       method: "get",
-//       url: "http://localhost:3000/users/7/events/6" + ".json",
-//       error: function(data) {
-//         $("#get_message").empty().append("Please try again.")
-//       },
-//       success: function(data) {
-//         var event = data.event;
-//           // debugger
-//           var id = event.id;
-//           var title = event.title;
-//           var userId = event.user_id;
-//           var eventLink = $(document.createElement("a"))
-//           var imageList = event.images
-//           imageList.forEach(function(image) {
-//             var title = image.title
-//
-//
-//             var imageLink = $(document.createElement("li"));
-//             // eventLink.attr('href', "http://localhost:3000/users/" + parseInt(userId) + "/events/" + parseInt(id))
-//             imageLink.text(image.title)
-//             $(".event").append(imageLink).append("<br>")
-//           })
-//         }
-//       })
-//     })
-//   }
+function showEvent(e) {
+  $(".all_events").on("click", function(e) {
+    e.preventDefault();
+    var eventId = e.target.href.split('/').splice(3,7)[3]
+    // debugger
+    $.ajax({
+      method: "get",
+      url: "http://localhost:3000/" + getRoute() + "/events/" + parseInt(eventId) + ".json",
+      error: function(data) {
+        $("#get_message").empty().append("Please try again.")
+      },
+      success: function(data) {
+        var event = data.event;
+          var eventId = event.id;
+          var eventTitle = event.title;
+          var userId = event.user_id;
+          var eventLink = $(document.createElement("a"))
+          var imageList = event.images
+          $(".event").empty();
+          imageList.forEach(function(image) {
+            var imageTitle = image.title
+            var imageId = image.id
+            var imageLink = $(document.createElement("a"));
+            imageLink.attr('href', "http://localhost:3000/events/" + parseInt(eventId) + "/images/" + parseInt(imageId))
+            imageLink.text(image.title)
+            $(".event").append(imageLink).append("<br>")
+          })
+        }
+      })
+    })
+  }
 
 
 function getRoute() {
@@ -69,26 +70,26 @@ function getRoute() {
 
 
 // Another anonymous practice get function
-$(function() {
-  $(".all_events").on("click", function(e) {
-
-    e.preventDefault();
-    $.get("http://localhost:3000/users/7/events/6" + getRoute() + ".json", function(data) {
-      var event = data.event;
-      var id = event.id;
-      var imageList = event.images
-      $(".event").empty();
-      imageList.forEach(function(image) {
-        var title = image.title
-        var imageId = image.id
-        var imageLink = $(document.createElement("a"));
-        imageLink.attr('href', "http://localhost:3000/events/" + parseInt(id) + "/images/" + parseInt(imageId))
-        imageLink.text(image.title)
-        $(".event").append(imageLink).append("<br>")
-      })
-    })
-  })
-})
+// $(function() {
+//   $(".all_events").on("click", function(e) {
+//
+//     e.preventDefault();
+//     $.get("http://localhost:3000/users/7/events/6" + getRoute() + ".json", function(data) {
+//       var event = data.event;
+//       var id = event.id;
+//       var imageList = event.images
+//       $(".event").empty();
+//       imageList.forEach(function(image) {
+//         var title = image.title
+//         var imageId = image.id
+//         var imageLink = $(document.createElement("a"));
+//         imageLink.attr('href', "http://localhost:3000/events/" + parseInt(id) + "/images/" + parseInt(imageId))
+//         imageLink.text(image.title)
+//         $(".event").append(imageLink).append("<br>")
+//       })
+//     })
+//   })
+// })
 
 // First anonymous practice get function
 // $(function() {
@@ -105,5 +106,5 @@ $(function() {
 
 $( document ).on('turbolinks:load', () => {
     getEvents();
-    // showEvent();
+    showEvent();
 })
